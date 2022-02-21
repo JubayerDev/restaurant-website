08755144b60ea6f7a738237d161cd8758e4497dd
JubayerDev <ajashes858@gmail.com>
2022-02-22 00:49:08 +0600

    newsletter created and footer running

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links-contact">
+
+      </div>
+      <div className="app__footer-links-logo">
+
+      </div>
+      <div className="app__footer-links_work">
+
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 - Jubayer All Rights Reserved</p>
+    </div>
   </div>
 );
 
